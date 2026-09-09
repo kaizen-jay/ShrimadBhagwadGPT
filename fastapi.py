99b256ef8f79a7f480c6b54b7f3cdd3ef0df0e37
Jay Raj <haldarjayraj@gmail.com>
2026-09-09 23:16:53 +0530

    Added the vision

---
+++ fastapi.py
@@ -0,0 +1,5 @@
+#This will be the main fastapi file but now its just a streamlit extension
+import streamlit as st
+
+st.header("BHAGWAD GPT - An app that resolves your problems based on Bhagwad Geeta")
+#I am trying to make an app that can take problem statements from the user and gives a Shrimad Bhagwad Geeta shlok as a solution
