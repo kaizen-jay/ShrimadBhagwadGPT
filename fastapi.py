90b4f5091f39ee242e2744ba042255b6c905c997
Jay Raj <haldarjayraj@gmail.com>
2026-09-16 23:55:14 +0530

    No contributions

--- fastapi.py
+++ fastapi.py
@@ -3,3 +3,6 @@ import streamlit as st
 
 st.header("BHAGWAD GPT - An app that resolves your problems based on Bhagwad Geeta")
 #I am trying to make an app that can take problem statements from the user and gives a Shrimad Bhagwad Geeta shlok as a solution.
+
+<h1> Procrastinator </h1>
+
